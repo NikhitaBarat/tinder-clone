bf9f036eb16b88dd1755b1d30344a88beaa1395f
Nikhita <bnikhita2003@gmail.com>
2022-04-08 17:05:47 +0530

    sec

--- tinder-backend/server.js
+++ tinder-backend/server.js
@@ -1,50 +1,45 @@
 import express from "express";
-import res from "express/lib/response";
 import mongoose from "mongoose";
-import Cards from "./dbcards";
+import Cards from "./dbcards.js";
 
 // app config
-const express = require('express')
 const app = express()
 const port = process.env.PORT || 3001
 
 const connection_url = `mongodb+srv://admin:sNKUbHe0C2S0NAl1@cluster0.anabo.mongodb.net/tinderdb?retryWrites=true&w=majority`
 
 // middleware
+app.use(express.json())
 
 //db config
 
-mongoose.connect(connection_url, {
-  useNewParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(connection_url)
 
 //api endpoints
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+  res.send('Hello World!')
+})
 
 app.post('/tinder/card', (req, res) => {
-    res.send('Got a POST request')
-    const dbCard = req.body;
+  res.send('Got a POST request')
+  const dbCard = req.body;
 
-    Cards.create(dbCard, (err, data) => {
-      if(err){
-        res.status(500).send(err)
-      }
-      else{
-        res.status(201).send(data)
-      }
-    })
-  });
+  Cards.create(dbCard, (err, data) => {
+    if (err) {
+      res.status(500).send(err)
+    }
+    else {
+      res.status(201).send(data)
+    }
+  })
+});
 
 app.get('/tinder/cards', (req, res) => {
   Cards.find((err, data) => {
-    if(err){
+    if (err) {
       res.status(500).send(err)
     }
-    else{
+    else {
       res.status(200).send(data)
     }
   })
@@ -52,5 +47,5 @@ app.get('/tinder/cards', (req, res) => {
 
 //listener
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+  console.log(`Example app listening on port ${port}`)
+})
--- tinder-clone/src/Tindercards.js
+++ tinder-clone/src/Tindercards.js
@@ -3,12 +3,36 @@ import './Tindercards.css'
 import TinderCard from 'react-tinder-card'
 function Tindercards() {
    const [people, setPeople] = useState([{
-    name: 'Aditya Roy Kapoor',
-    url: "https://www.koimoi.com/wp-content/new-galleries/2022/01/when-aditya-roy-kapur-received-a-love-letter-by-a-jabra-fan-which-was-covered-in-blood-heres-how-he-reacted-001.jpg"
+    name: 'William Abadie',
+    url: "https://media1.popsugar-assets.com/files/thumbor/bP8iM4mIR-5YQm1_FFP2kEAlNpc/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2020/10/05/751/n/1922283/845970adb121519d_EMILYINPARIS_Season1_Episode3_00_09_02_15R/i/William-Abadie-as-Antoine-on-Emily-in-Paris.jpg"
    },
    {
        name: 'Reneé Rapp',
-       url: "https://artworks.thetvdb.com/banners/v4/actor/8181659/photo/6196f5ff8ad03.jpg"
+       url: "https://s.abcnews.com/images/Entertainment/WireAP_3d1760085bd7456fabec68221b383944_16x9_992.jpg"
+   },
+   {
+       name: 'Miguel Herrán',
+       url: "https://img.allvipp.com/www-promipool-de/image/upload/c_fill,g_faces,w_1200,h_1200,q_auto:eco,f_jpg/money_heist_this_is_miguel_herran_200424_ge53jl5jd6"
+   },
+   {
+       name: 'Alyah Chanelle Scott',
+       url: "https://static.cdn.turner.com/styles/scale_792/s3/images/2021-11/alyah-chanelle-scott_5.jpg?itok=UaIOAxY5"
+   },
+   {
+       name: 'Antonio Cipriano',
+       url: "https://static.onecms.io/wp-content/uploads/sites/20/2021/09/26/jagged-little-pill-antonio-cipriano-celia-rose-gooding.jpg"
+   },
+   {
+       name: 'Ashley Park',
+       url: "https://www.theintervalny.com/wp-content/uploads/2018/05/crop-3.jpg"
+   },
+   {
+       name: 'Lucas Bravo',
+       url: "https://www.vizaca.com/wp-content/uploads/2021/12/Lucas-Bravo-%E2%80%98The-Man-Behind-Gabriel.jpg"
+   },
+   {
+       name: 'Lucien Laviscount',
+       url: "https://images.indianexpress.com/2015/02/luscien-laviscount-759.jpg"
    },
    {
        name: 'Gavin Leatherwood',
